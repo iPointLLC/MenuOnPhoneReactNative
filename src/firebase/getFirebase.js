9f4e03bc2cbfa296cbd43c70a7f5ec760fa6ebdd
Aleksei Danilov <danilovdev@gmail.com>
2017-12-04 22:03:49 +0800

    fix firebase

--- src/firebase/getFirebase.js
+++ src/firebase/getFirebase.js
@@ -3,12 +3,12 @@ import * as firebase from 'firebase';
 let FirebaseApp;
 
 const config = {
-	apiKey: 'AIzaSyADrRoDJfcCyMgiiIlefYXYab-69h2__Eo',
-	authDomain: 'menuonphone-3ad64.firebaseapp.com',
-	databaseURL: 'https://menuonphone-3ad64.firebaseio.com',
-	projectId: 'menuonphone-3ad64',
-	storageBucket: 'menuonphone-3ad64.appspot.com',
-	messagingSenderId: '868452526664'
+	apiKey: 'apiKey',
+	authDomain: 'authDomain',
+	databaseURL: 'databaseURL',
+	projectId: 'projectId',
+	storageBucket: 'storageBucket',
+	messagingSenderId: 'messagingSenderId'
 };
 
 export const getFirebase = () => {
